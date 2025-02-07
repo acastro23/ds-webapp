5cc52bb2b54434ef6c32e655c7c852af42d52748
Alex Castro <alexscastro2002@gmail.com>
2025-02-07 16:21:09 -0500

    AC02072025 -- Made edits to the 'heap-sort.html' notes.

--- learnApp/views.py
+++ learnApp/views.py
@@ -20,6 +20,41 @@ bubble_sort(arr)
 print(arr)
         """,
         "step_images": []
+    },
+
+    "heap-sort": {
+        "title": "Heap Sort",  
+        "code": """def Heapify(arr, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+
+    if l < n and arr[i] < arr[l]:
+        largest = l
+
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        Heapify(arr, n, largest)
+
+def HeapSort(arr):
+    n = len(arr)
+
+    for i in range(n // 2, -1, -1):
+        Heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        Heapify(arr, i, 0)
+
+# Sample output
+arr = [4, 3, 11, 9, 12, 10, 2, 5, 7, 6, 1, 8]
+HeapSort(arr)
+print("Sorted array is:", arr)
+        """,
+        "step_images": []
     }
 }
 
